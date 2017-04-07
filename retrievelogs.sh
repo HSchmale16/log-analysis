aca0301304e38dae3bcd5171505c5a0dbd5901af
Henry J Schmale <hschmale16@gmail.com>
2017-04-07 14:40:12 -0400

    Break out log retrieval

---
+++ retrievelogs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# gets the logs
+
+# ssh connection to sftp files from
+SSHLocation=hjsblog
+# Location and pattern of logs
+GetLogs=../logs/access*
+
+
+#######################################
+#######################################
+
+CWD=$(pwd)
+
+# move to get location
+mkdir -p $PlaceLogs
+cd $PlaceLogs
+
+# perform the get operation
+sftp $SSHLocation << EOF
+cd $(dirname $GetLogs)
+get $(basename $GetLogs)
+EOF
+
+# uncompress files
+bunzip2 *.bz2
+
+# return to script dir
+cd $CWD
