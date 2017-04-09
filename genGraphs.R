1e22d9f8811ec096b7de4c4be18002bd96cd7eee
Henry J Schmale <hschmale16@gmail.com>
2017-04-09 11:09:22 -0400

    Work on the graph generation

--- genGraphs.R
+++ genGraphs.R
@@ -5,28 +5,38 @@ library(ggplot2)
 library(rjson)
 library(reshape2)
 
+# load the views of each article
 articleViewCount <- data.frame(
   read.csv('articleViews.csv', header = FALSE))
+
+# load postHitCount or the popularity of tag hits with usage
 postHitCount <- data.frame(
   read.csv('postHits.csv', header = FALSE))
-postHitCount$V4 <- as.numeric(postHitCount$V2) / postHitCount$V3
-postTags <- fromJSON(file = 'posttags.json')
+postHitCount$HitsVUse <- as.numeric(postHitCount$V2) / postHitCount$V3
+names(postHitCount) <- c('tagname', 'hitcnt', 'usecnt', 'usevhit')
 
 
+# load the post tags file, as it contains all of the tags
+articleTags <- fromJSON(file = 'posttags.json')
 
+
+# Plot the actual views of each article
 articleViewsPlot <- ggplot(data=articleViewCount, aes(x=V1, y=V2)) +
   xlab('post title') + ylab('views') +
   ggtitle('Views of Each Post') +
   geom_bar(stat="identity") +
   theme(axis.text = element_text(angle=75, hjust = 1))
 
-ggplot(data=postHitCount, aes(V3, fill = V1)) +
-  xlab('tag name') + ylab('tag views') +
-  ggtitle('Tag Views') +
-  geom_bar(stat = 'identity') +
-  theme(axis.text = element_text(angle = 75, hjust = 1))
+# Plot the use of tags of articles
+df <- melt(postHitCount)
+ggplot(df, aes(x = tagname, y = value)) +
+  geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
+  scale_y_log10() +
+  theme(axis.text = element_text(angle=75, hjust = 1))
 
-hitTagsPerPostView <- ggplot(data=postHitCount, aes(x = V1, y = V4)) +
+
+
+hitTagsPerPostView <- ggplot(data=postHitCount, aes(x=tagname, y = usevhit)) +
   xlab('tag name') + ylab('tag count / post count with tag') +
   ggtitle('Tag Hits / Post Views') +
   geom_bar(stat = 'identity') +
