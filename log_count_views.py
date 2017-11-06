c543e5375c28f3d4c5ab18b3c787f1e1d448191c
Henry J Schmale <hschmale16@gmail.com>
2017-11-05 19:08:04 -0500

    Filter Out Bots in Counting Post Views

--- log_count_views.py
+++ log_count_views.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 # Counts occurances of a post url and counts them up.
 
+import sys
+import re
 import apache_log_parser
 from collections import Counter
-from pprint import pprint
-import sys
 
 line_parser = apache_log_parser.make_parser("%a - %u %t \"%m %U %H\" %s %B \"%{Referer}i\" \"%{User-Agent}i\"")
+bad_ua = re.compile('([Bb]ot|[Ss]pider|[Cc]rawler)')
 
+"""
+Loads the url_path in all log files
+"""
 def handleLogFile(x):
     l = list()
     with open(x) as f:
         for x in f:
             try:
                 z = line_parser(x.rstrip())
-                l.append(z['url_path'])
+                if bad_ua.search(z['request_header_user_agent']) is not None:
+                    l.append(z['url_path'])
             except:
                 pass
     return l
@@ -24,7 +29,7 @@ urls = map(handleLogFile, sys.argv[1:])
 c = Counter()
 for x in urls:
     for y in x:
-        if(y.startswith('/2') and y.endswith('html')):
+        if(y.startswith('/20') and y.endswith('html')):
             c[y] += 1
 for x in c:
     print(x + ',' + str(c[x]).lower())
