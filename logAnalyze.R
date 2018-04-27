ef8cb1c8c3730a6fb3cbc41e642664a92fe78b29
Henry J Schmale <hschmale16@gmail.com>
2018-04-27 16:13:35 -0400

    Do Things

--- logAnalyze.R
+++ logAnalyze.R
@@ -15,16 +15,26 @@ livePosts <- names(tags)
 hitCounts <- read.csv(file = 'articleViews.csv', header = FALSE)
 names(hitCounts) <- c('path', 'date', 'hits')
 hitCounts$date <- as.Date(hitCounts$date)
-
 livePostHit <- hitCounts[hitCounts$path %in% livePosts,]
+
+# Total Number Of Hits
+totalHits <- livePostHit %>%
+  group_by(path) %>%
+  summarise(hits=sum(hits))
+
+ggplot(totalHits, aes(x = path, y = hits)) +
+  geom_bar(stat = 'identity') +
+  theme(axis.text = element_text(angle=75, hjust = 1)) +
+  ggtitle("Total Views of Posts")
+
+# Grouped By Month
 summarizedPostHit<- livePostHit %>%
   group_by(path, month=floor_date(date, "month")) %>%
   summarize(hits=sum(hits))
 
-
 ggplot(summarizedPostHit, aes(x = path, y = month, fill = hits)) +
   geom_tile() +
   coord_flip() +
-  ggtitle("Post Hits Over Time") +
-  scale_fill_continuous(low='white', high='red')
+  ggtitle("Post Hits Over Time Grouped By Month") +
+  scale_fill_continuous(low='white', high='green')
 
