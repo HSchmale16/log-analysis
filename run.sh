f99f3f45d3ef06afc208a5c5898df99601d91b71
Henry Schmale <hschmale16@gmail.com>
2020-11-30 18:33:03 -0500

    Run script will print out new views
    Views are calculated using the articleViewCsv. The log_count_views.py script
    computes that.

--- run.sh
+++ run.sh
@@ -9,7 +9,17 @@ ArticleViewCsv=articleViews.csv
 # Post Hit Count CSV
 PostHitCountCsv=postHits.csv
 
+function countViewsFromCsv() {
+    [[ -f "$ArticleViewCsv" ]] && \
+        awk -F, '{s+=$3}END{print s}' $ArticleViewCsv || echo 0
+}
+
 
 # run the log analysis
+BEFORE_VIEW_COUNT=$(countViewsFromCsv)
 ./log_count_views.py $PlaceLogs/* > $ArticleViewCsv
+AFTER_VIEW_COUNT=$(countViewsFromCsv)
+
+echo "Got $((AFTER_VIEW_COUNT - BEFORE_VIEW_COUNT)) new article views"
+
 ./make_plots.R
